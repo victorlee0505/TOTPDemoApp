0eccb728f1a6c05e6f833c6f017a5c85e4285bc1
Victor Lee <victorlee0505@gmail.com>
2021-06-23 15:22:26 -0400

    first commit

---
+++ src/main/java/com/example/security/totpdemoapp/TotpdemoappApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.totpdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotpdemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotpdemoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security/totpdemoapp/TotpdemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security.totpdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotpdemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
